ef66698d25337d429d98e9ef37b8d2995b004004
mohammed-ibrahim <mohammed.ibrahim@practo.com>
2018-06-11 17:24:57 +0530

    Adding timer

--- csv_open.py
+++ csv_open.py
@@ -8,7 +8,7 @@ from whoosh.qparser import QueryParser
 import whoosh.index as index
 import uuid
 import signal
-import enum
+import datetime
 import readline
 import hashlib
 
@@ -25,6 +25,18 @@ def setup_logging():
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_formatter)
     root_logger.addHandler(console_handler)
+
+
+class Timer():
+
+    _start_time = None
+    def __init__(self):
+        self._start_time = datetime.datetime.now()
+
+    def end(self):
+        end_time = datetime.datetime.now()
+        delta = end_time - self._start_time
+        return str(delta)
 
 # __________      .__.__       .___ ________                                             __
 # \______   \__ __|__|  |    __| _/ \______ \   ____   ____  __ __  _____   ____   _____/  |_
@@ -85,159 +97,28 @@ def index_csv_file(reader, index_location):
             add_record(index_writer, headers, row)
             num_documents = num_documents + 1
 
+            if num_documents > 1 and num_documents % 500 == 0:
+                log.info("Added documents: %d", num_documents)
+
     log.info("Documents added: %d", num_documents)
     return (headers, index_doc)
 
 
-# ________                                  _____                .__
-# \_____  \  __ __   ___________ ___.__.   /  _  \   ____ _____  |  | ___.__.________ ___________
-#  /  / \  \|  |  \_/ __ \_  __ <   |  |  /  /_\  \ /    \\__  \ |  |<   |  |\___   // __ \_  __ \
-# /   \_/.  \  |  /\  ___/|  | \/\___  | /    |    \   |  \/ __ \|  |_\___  | /    /\  ___/|  | \/
-# \_____\ \_/____/  \___  >__|   / ____| \____|__  /___|  (____  /____/ ____|/_____ \\___  >__|
-#        \__>           \/       \/              \/     \/     \/     \/           \/    \/
-
-class Operation(enum.Enum):
-    EXIT = 1
-    SHOW_FIELDS = 2
-    ALL_TERMS = 3
-    TERMS = 4
-    GEN_QUERY = 5
-    NONE = 6
-    ERROR = 7
-
-class QueryAnalyzer():
-
-    _successful = False
-    _operation = None
-    _params = []
-    _q = None
-    _message = None
-
-    def __init__(self, query):
-        self._q = query
-        self.analyze(query)
-
-    def param(self, index):
-        return self._params[index]
-
-    def analyze(self, original_query):
-        query = original_query.lower().strip()
-
-        if query == "exit":
-            self._operation = Operation.EXIT
-
-        elif query == "desc":
-            self._operation = Operation.SHOW_FIELDS
-
-        elif query == "all_terms":
-            self._operation = Operation.ALL_TERMS
-
-        elif query.startswith("terms"):
-            self._operation = Operation.TERMS
-            parts = query.split(" ")
-            if len(parts) != 2:
-                self._operation = Operation.ERROR
-                self._message = "Usage: terms field_name"
-            else:
-                self._params = parts
-
-        elif query == "":
-            self._operation = Operation.NONE
-
-        else:
-            self._operation = Operation.GEN_QUERY
-            self._params = query.split(" ")
-
-
-#   _________                           .__         _____  .__
-#  /   _____/ ____ _____ _______   ____ |  |__     /  _  \ |  |    ____   ____
-#  \_____  \_/ __ \\__  \\_  __ \_/ ___\|  |  \   /  /_\  \|  |   / ___\ /  _ \
-#  /        \  ___/ / __ \|  | \/\  \___|   Y  \ /    |    \  |__/ /_/  >  <_> )
-# /_______  /\___  >____  /__|    \___  >___|  / \____|__  /____/\___  / \____/
-#         \/     \/     \/            \/     \/          \/     /_____/
-def perform_search(headers, index_doc, query):
-    parts = query.split(" ", 1)
-    field = parts[0]
-    value = parts[1]
-
-    with index_doc.searcher() as searcher:
-        qp = QueryParser(field, schema=index_doc.schema)
-        q = qp.parse(unicode(query))
-        results = searcher.search(q)
-
-        for result in results:
-            for header in headers:
-                if header in result:
-                    log.info("%s: %s", header, result[header].encode("utf-8"))
-                else:
-                    log.info("%s: NA", header)
-
-
-        log.info("Total results: %d\n", len(results))
-
-    return None
-
-# .___        __                              __  .__
-# |   | _____/  |_  ________________    _____/  |_|__| ____   ____
-# |   |/    \   __\/ __ \_  __ \__  \ _/ ___\   __\  |/  _ \ /    \
-# |   |   |  \  | \  ___/|  | \// __ \\  \___|  | |  (  <_> )   |  \
-# |___|___|  /__|  \___  >__|  (____  /\___  >__| |__|\____/|___|  /
-#          \/          \/           \/     \/                    \/
-def iterative_search(headers, index_doc):
-
-    while True:
-        user_input = raw_input("Enter query: ")
-        parsed_query = QueryAnalyzer(user_input)
-        log.info("Operation: %s", str(parsed_query._operation))
-        log.info("")
-
-        if parsed_query._operation == Operation.EXIT:
-            log.info("Bye..")
-            break
-
-        if parsed_query._operation == Operation.SHOW_FIELDS:
-            for header in headers:
-                log.info(header)
-            continue
-
-        if parsed_query._operation == Operation.ALL_TERMS:
-            with index_doc.searcher() as searcher:
-                for header in headers:
-                    log.info("Values for: %s", header)
-                    log.info(list(searcher.lexicon(header)))
-            continue
-
-        if parsed_query._operation == Operation.NONE:
-            continue
-
-        if parsed_query._operation == Operation.ERROR:
-            log.info(parsed_query._message)
-            continue
-
-        if parsed_query._operation == Operation.TERMS:
-            with index_doc.searcher() as searcher:
-                log.info("Values for: %s", parsed_query.param(1))
-                log.info(list(searcher.lexicon(parsed_query.param(1))))
-            continue
-
-        perform_search(headers, index_doc, parsed_query._q)
-
-    return None
-
-# def signal_handler(signal, frame):
-#     print 'Exiting...'
-#     sys.exit(0)
-
 def md5(fname):
+    md5_timer = Timer()
+
     hash_md5 = hashlib.md5()
     with open(fname, "rb") as f:
         for chunk in iter(lambda: f.read(4096), b""):
             hash_md5.update(chunk)
 
+    log.info("Time taken to compute md5 of file: %s is : %s", fname, md5_timer.end())
+
     return hash_md5.hexdigest()
 
 
 def get_headers_of_csv(input_csv_file):
+
     headers = None
     with open(input_csv_file, 'rb') as file_pointer:
         reader = csv.reader(file_pointer)
@@ -262,6 +143,7 @@ if __name__ == "__main__":
     raw_query = sys.argv[2]
     result_headers = sys.argv[3].split(",")
 
+    op_timer = Timer()
     input_csv_file_full_path = os.path.abspath(input_csv_file)
     file_name_md5 = hashlib.md5(input_csv_file_full_path).hexdigest()
     file_contents_md5 = md5(input_csv_file_full_path)
@@ -281,6 +163,7 @@ if __name__ == "__main__":
             (headers, index_handle) = index_csv_file(reader, index_location)
 
     print("\n\n")
+
     with index_handle.searcher() as searcher:
         qp = QueryParser('default_field', schema=index_handle.schema)
         q = qp.parse(unicode(raw_query))
@@ -301,4 +184,4 @@ if __name__ == "__main__":
 
             print(",".join(resp_doc_buffer))
 
-        log.info("\nTotal results: %d", len(results))
+        log.info("\nTotal results: %d, Time taken: %s", len(results), op_timer.end())
